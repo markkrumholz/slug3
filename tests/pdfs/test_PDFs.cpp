c1904e3ea316dd4ec41e9e0e07d10dabe84381d1
Mark Krumholz <mark.krumholz@anu.edu.au>
2026-06-13 13:16:06 +1000

    Add lognormal segment type

--- tests/pdfs/test_PDFs.cpp
+++ tests/pdfs/test_PDFs.cpp
@@ -3,13 +3,14 @@
  * @author Mark Krumholz
  * @brief Unit tests for the PDF classes.
  * @details
- * This file contains unit tests for the PDF classes defined in the src/pdfs directory, including PDFSegmentDelta, PDFSegmentExponential, PDFSegmentNormal, PDFSegmentPowerlaw, and PDFSegmentSchechter. The tests cover the evaluation of the PDF at specific points, the calculation of expectation values and integrals over specified ranges, and the sampling of random values from the distributions.
+ * This file contains unit tests for the PDF classes defined in the src/pdfs directory, including PDFSegmentDelta, PDFSegmentExponential, PDFSegmentNormal, PDFSegmentLognormal, PDFSegmentPowerlaw, and PDFSegmentSchechter. The tests cover the evaluation of the PDF at specific points, the calculation of expectation values and integrals over specified ranges, and the sampling of random values from the distributions.
  * @date 2024-06-12
  */
 
 #include "test_PDFSegmentDelta.hpp"
 #include "test_PDFSegmentExponential.hpp"
 #include "test_PDFSegmentNormal.hpp"
+#include "test_PDFSegmentLognormal.hpp"
 #include "test_PDFSegmentPowerlaw.hpp"
 #include "test_PDFSegmentSchechter.hpp"
 
@@ -18,6 +19,7 @@ int main() {
     result += test_PDFSegmentDelta();
     result += test_PDFSegmentExponential();
     result += test_PDFSegmentNormal();
+    result += test_PDFSegmentLognormal();
     result += test_PDFSegmentPowerlaw();
     result += test_PDFSegmentSchechter();
     return result;
