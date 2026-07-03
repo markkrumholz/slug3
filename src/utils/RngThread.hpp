d134afb484e28ce7c93512695d8163de23f5de21
Mark Krumholz <mark.krumholz@anu.edu.au>
2026-07-03 18:09:01 +1000

    Linting

--- src/utils/RngThread.hpp
+++ src/utils/RngThread.hpp
@@ -39,12 +39,13 @@ namespace utils {
          */
         RngThread()
         {
-            for (auto& r : rngEngines)
+            for (auto& r : rngEngines_)
             {
-                pcg_extras::seed_seq_from<std::random_device> seed_source;
-                r = std::make_unique<RngType>(seed_source);
+                pcg_extras::seed_seq_from<std::random_device> seedSource;
+                r = std::make_unique<RngType>(seedSource);
             }
         }
+
         /**
          * @brief Construct an RngThread with a specified seed
          * @param seed The seed value to use
@@ -55,12 +56,19 @@ namespace utils {
          */
         RngThread(const RngType::state_type seed)
         {
-            for (size_t i = 0; i < rngEngines.size(); ++i)
+            for (int i = 0; i < rngEngines_.size(); ++i)
             {
-                rngEngines[i] = std::make_unique<RngType>(seed + i);
+                rngEngines_[i] = std::make_unique<RngType>(seed + i); // NOLINT(cppcoreguidelines-pro-bounds-avoid-unchecked-container-access)
             }
         }
+
         ~RngThread() = default;
+        
+        // Disable copy and move constructors and assignment operators
+        RngThread(const RngThread&) = delete;
+        RngThread(RngThread&&) = delete;
+        auto operator=(const RngThread&) -> RngThread& = delete;
+        auto operator=(RngThread&&) -> RngThread& = delete;
 
         /**
          * @brief Re-seed the random number generator
@@ -73,9 +81,9 @@ namespace utils {
          */
         void seed(const RngType::state_type seed)
         {
-            for (size_t i = 0; i < rngEngines.size(); ++i)
+            for (int i = 0; i < rngEngines_.size(); ++i)
             {
-                rngEngines[i]->seed(seed + i);
+                rngEngines_[i]->seed(seed+i); // NOLINT(cppcoreguidelines-pro-bounds-avoid-unchecked-container-access)
             }
         }
 
@@ -83,17 +91,18 @@ namespace utils {
          * @fn utils::RngThread::operator()() const
          * @brief Return the correct rng engine for the calling thread
          */
-        auto operator()() const -> RngType& { return *rngEngines(); }
+        auto operator()() const -> RngType& { return *rngEngines_(); }
 
     private:
 
-        ThreadVec<std::unique_ptr<pcg64> > rngEngines;  /**< Pointers to rng engines */
+        ThreadVec<std::unique_ptr<pcg64> > rngEngines_;  /**< Pointers to rng engines */
 
     };
 
     // Create a static instance of an RngThread object,
-    // which will be usable from throughout the code
-    static RngThread rng;
+    // which will be usable from throughout the code. This ensures
+    // that all random numbers are handled in a thread-safe manner.
+    static RngThread rng; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 } // namespace utils
 
