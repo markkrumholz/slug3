987640c73ca003423b7f33a9e63913590e72430f
Mark Krumholz <mark.krumholz@anu.edu.au>
2026-07-19 22:08:53 +1000

    Add command line arguments

--- data/tools/fetch_bosz.py
+++ data/tools/fetch_bosz.py
@@ -6,6 +6,7 @@ gzip'ed HDF5 file that slug can read.
 
 # Imports
 import argparse
+import shutil
 
 # Magic strings
 BOSZ_version = "2024"
@@ -17,3 +18,31 @@ BOSZ_refernce_URLS = ["https://ui.adsabs.harvard.edu/abs/2012AJ....144..120M/abs
                       "https://ui.adsabs.harvard.edu/abs/2017AJ....153..234B/abstract",
                       "https://ui.adsabs.harvard.edu/abs/2024A%26A...688A.197M/abstract",
                       "https://dx.doi.org/10.17909/T95G68"]
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Fetch BOSZ spectral library")
+parser.add_argument("--version", default=BOSZ_version, 
+                    help="BOSZ version to fetch")
+parser.add_argument("--url", default=BOSZ_URL, 
+                    help="URL of the BOSZ data")
+parser.add_argument("--output", 
+                    default=shutil.os.path.join("..", "spectra", "bosz.h5"), 
+                    help="Output file for the HDF5 spectra")
+parser.add_argument("--registry", 
+                    default=shutil.os.path.join("..", "spectra", "spectra.toml"), 
+                    help="Output file for the registry")
+parser.add_argument("--overwrite", action="store_true",
+                    help="Overwrite existing output file")
+parser.add_argument("--feh", type=float, nargs="+", default=[],
+                    help="List of [Fe/H] values to fetch; if unspecified, fetch all")
+parser.add_argument("--afe", type=float, nargs="+", default=[],
+                    help="List of [alpha/Fe] values to fetch; if unspecified, fetch all")
+parser.add_argument("--cfe", type=float, nargs="+", default=[],
+                    help="List of [C/Fe] values to fetch; if unspecified, fetch all")
+parser.add_argument("--r", type=int, nargs="+", default=[],
+                    help="List of r values to fetch; if unspecified, fetch all")
+parser.add_argument("--micro", type=int, nargs="+", default=[],
+                    help="List of microturbulent vleocity values to fetch; if unspecified, fetch all")
+parser.add_argument("--verbose", action="store_true",
+                    help="Print verbose output")
+args = parser.parse_args()
